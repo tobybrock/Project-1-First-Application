ad0cb7667d6a14fe084782a900f179ffe3998f67
tobybrock <tobybrocklehurst6@gmail.com>
2020-05-28 23:22:05 +1000

    Creating hide and show functionality with the jumbotron and carousel respectively

--- app.js
+++ app.js
@@ -1,37 +1,52 @@
 
-getPicture();
+//GET weather api
 async function getWeather(city){
     const response = await $.ajax(`https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=3f83b809a68640b8957ad8619c559f88`)
     .catch(e => console.log(e));
+    //append data to carousel
     fortnightForecast(response.data);
     console.log(response.data[0]);
     
  }
 
+ //GET Image API
+ //the condition variable is a weather description from the getWeather API
  async function getPicture(condition){
      const response = await $.ajax(`https://pixabay.com/api/?key=16762787-98934c07282a01a02ff313326&q=${condition}&image_type=photo`)
      .catch(e => console.log(e));
-     console.log(response.imageURL);
+     //console.log(response.hits[0].webformatURL);
+     return(response.hits[0].webformatURL);
+     
  }
-
+//On Ready Function
 $(() => {
 $("#cityBtn").on('click', (e) => {
     let city = $("#cityInput").val();
-    hideJumbo();
-    getWeather(city);
+    
     carousel();
-})    
-})
+    getWeather(city);
+    
+});
+hideJumbo();    
+showCarousel();
+});
 
+//This function appends API data gathered from the weather and image APIs to an unordered list inside a carousel
 function fortnightForecast(data) {
     const weatherData = $(".slides");
     for(let i =0; i < 14; i++){
-        weatherData.append(`<li class="slide">${data[i].datetime} ${data[i].temp}C ${data[i].weather.description}</li>`);
-        console.log(data[i]);
-        
+        weatherData.append(`<li class="slide">${data[i].datetime} ${data[i].temp} ${data[i].weather.description}</li>`);
+        console.log(data[i].weather.description);
+        //Use weather description as the condition in GetPicture
+        /*let weatherDescription = (data[i].weather.description);
+        let replaced = weatherDescription.split(' ').join('+');
+        let pictureUrl = getPicture(replaced);
+        console.log(getPicture(replaced));
+        weatherData.append(`<li class="slideImage"><img src=${pictureUrl}></li>`);
+        */
     }
 }
-
+//Create a carousel
 function carousel() {
     const width = 720;
     const animationSpeed = 1000;
@@ -40,6 +55,7 @@ function carousel() {
     const slideContainer =carousel.find(".slides");
     const slides = $('.slide');
     
+    //animate on click to move the carousel right
    $(carouselBtnRight).on('click', (() =>{
         slideContainer.animate({"margin-left": "-=" + width}, animationSpeed, (() => {
             currentSlide ++;
@@ -47,6 +63,7 @@ function carousel() {
         }));
     }));
    
+     //animate on click to move the carousel left
     $(carouselBtnLeft).on('click', (() =>{
         slideContainer.animate({"margin-left": "+=" + width}, animationSpeed, (() => {
             currentSlide --;
@@ -58,6 +75,25 @@ function carousel() {
     }));
 
 }
+//hide Jumbotron after search is clicked **will need styling
 function hideJumbo(){
-    $("#searchCityJumbo").hide();
+    $("#cityBtn").on('click', (() => {
+        let jumbo = $(".searchCityJumbo");
+        jumbo.hide();
+
+    }));
+    
+}
+
+//show the carousel after search is clicked **will need styling
+function showCarousel() {
+    $("#cityBtn").on('click', (() => {
+        let carousel = $('#carousel');
+        let btnLeft = $("#carouselBtnLeft");
+        let btnRight = $("#carouselBtnRight");
+        carousel.show();
+        btnLeft.show();
+        btnRight.show();
+        
+    }));
 }
